12d788714b7e847f6683d3cd8986f656a3a04f93
Rksingh108 <150592731+Rksingh108@users.noreply.github.com>
2023-12-02 21:37:50 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,11 +1,16 @@
-const http = require("http");
-const fs = require("fs");
-const { register } = require("module");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
 
 let homeContent = "";
 let projectContent = "";
 let registrationContent = "";
 
+// Use the supplied port or default to 3000
+const port = argv.port ? parseInt(argv.port) : 3000;
+
+// Read files asynchronously and store their content
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
@@ -13,11 +18,11 @@ fs.readFile("home.html", (err, home) => {
   homeContent = home;
 });
 
-fs.readFile("registration.html",(err, registration) => {
-    if (err){
-        throw err;
-    }
-    registrationContent = registration;
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
 });
 
 fs.readFile("project.html", (err, project) => {
@@ -26,25 +31,28 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-        case "/registration":
-            response.write(registrationContent);
-            response.end();
-            break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(5000);
 
+// Create an HTTP server
+const server = http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHeader(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      response.write(projectContent);
+      response.end();
+      break;
+    case "/registration":
+      response.write(registrationContent);
+      response.end();
+      break;
+    default:
+      response.write(homeContent);
+      response.end();
+      break;
+  }
+});
 
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
